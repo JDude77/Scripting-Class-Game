3ec574e9ee021fb11772988953b8d1bea86c0d3e
Jordan (JDude77) <jordy@oaksydix.com>
2020-03-13 01:28:00 +0000

    Fresher start

--- Scripting Class Game/Assets/Scripts/Player/MouseLook.cs
+++ Scripting Class Game/Assets/Scripts/Player/MouseLook.cs
@@ -9,7 +9,6 @@ public class MouseLook : MonoBehaviour
     private Transform playerBody;
     private float xRotation = 0f;
     private GameObject player;
-    private bool swivel;
     #endregion
 
     #region Getters & Setters
@@ -38,18 +37,6 @@ public class MouseLook : MonoBehaviour
         playerBody = newPlayerBody;
     }//End player body setter
     #endregion
-    #region Swivel
-    //Swivel Getter
-    public bool getSwivel()
-    {
-        return swivel;
-    }//End Swivel Getter
-    //Swivel Setter
-    public void setSwivel(bool swivel)
-    {
-        this.swivel = swivel;
-    }//End Swivel Setter
-    #endregion
     #endregion
 
     //Start is called before the first frame update
@@ -63,25 +50,12 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (!player.GetComponent<PlayerInteraction>().getIsInteracting())
-        {
-            swivel = false;
             //Get movement of mouse according to mouse sensitivity and make sure it's the same across framerate
             float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
             float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
             rotatePlayerHorizontal(mouseX);
             rotateCameraVertical(mouseY);
-        }//End if
-        else
-        {
-            Cursor.lockState = CursorLockMode.None;
-        }//End else
-
-        if(swivel)
-        {
-            swivelCamera(player.GetComponent<PlayerInteraction>().getOther());
-        }//End if
     }//End Update
 
     #region Behaviours
@@ -100,34 +74,5 @@ public class MouseLook : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }//End rotateCameraVertical
-
-    //Make player directly face whatever they're interacting with
-    public void swivelCamera(GameObject other)
-    {
-        //Get the camera's transform
-        Transform cam = GetComponentInChildren<Camera>().gameObject.transform;
-        //Get the other object's transform
-        Transform target = other.transform;
-        //If the angle isn't facing the target yet
-        if (Vector3.Angle(cam.forward, target.position - cam.position) > 1f)
-        {
-            Transform current = playerBody;
-            current.rotation = Quaternion.Lerp(playerBody.rotation, Quaternion.LookRotation(target.position - playerBody.position), 2 * Time.deltaTime);
-            Vector3 euler = current.rotation.eulerAngles;
-            euler.x = 0;
-            euler.z = 0;
-            player.transform.rotation = Quaternion.Euler(euler);
-            //Lerp between the current camera's transform and the target transform
-            current = cam.transform;
-            current.rotation = Quaternion.Lerp(cam.rotation, Quaternion.LookRotation(target.position - cam.position), 2 * Time.deltaTime);
-            cam.LookAt(current);
-        }//End if
-        //If the angle is close enough to the target
-        else
-        {
-            //Stop moving the camera
-            swivel = false;
-        }//End else
-    }//End swivelCamera
     #endregion
 }
